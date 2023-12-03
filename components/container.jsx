4cf51feca9b0b8a0c16be4500d96514a6ee7fbfd
Irfan Muqorib <irfanmuqorib33@gmail.com>
2023-12-03 15:13:49 +0700

    update: layouting

--- components/container.jsx
+++ components/container.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Container = ({ children }) => {
     return (
-        <div className='pt-40 pb-10 mx-4 md:mx-10 lg:mx-20 lg:pb-20'>
+        <div className='pt-20 pb-10 mx-4 md:pt-32 xl:pt-40 md:mx-10 lg:mx-20 lg:pb-20'>
             {children}
         </div>
     )
